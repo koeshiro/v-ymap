e5b8d64a77f277370332f1f40592ca54f9d9eeaf
koeshiro <koeshiro@yandex.ru>
2020-12-30 16:36:46 +0300

    Rewrite components names

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+import './src/components/index.css'
 export function install(Vue, options){
-    Vue.component('v-yamp', ()=> import("./src/components/index"));
-    Vue.component('v-yamp-collection', ()=> import("./src/components/collection"));
-    Vue.component('v-yamp-clusterer', ()=> import("./src/components/clusterer"));
-    Vue.component('v-yamp-circle', ()=> import("./src/components/geo-objects/circle"));
-    Vue.component('v-yamp-placemark', ()=> import("./src/components/geo-objects/placemark"));
-    Vue.component('v-yamp-polygon', ()=> import("./src/components/geo-objects/polygon"));
-    Vue.component('v-yamp-polyline', ()=> import("./src/components/geo-objects/polyline"));
-    Vue.component('v-yamp-rectangle', ()=> import("./src/components/geo-objects/rectangle"));
-    Vue.component('v-yamp-route', ()=> import("./src/components/geo-objects/route"));
+    Vue.component('v-ymap', ()=> import("./src/components/index"));
+    Vue.component('v-ymap-collection', ()=> import("./src/components/collection"));
+    Vue.component('v-ymap-clusterer', ()=> import("./src/components/clusterer"));
+    Vue.component('v-ymap-circle', ()=> import("./src/components/geo-objects/circle"));
+    Vue.component('v-ymap-placemark', ()=> import("./src/components/geo-objects/placemark"));
+    Vue.component('v-ymap-polygon', ()=> import("./src/components/geo-objects/polygon"));
+    Vue.component('v-ymap-polyline', ()=> import("./src/components/geo-objects/polyline"));
+    Vue.component('v-ymap-rectangle', ()=> import("./src/components/geo-objects/rectangle"));
+    Vue.component('v-ymap-route', ()=> import("./src/components/geo-objects/route"));
 }
