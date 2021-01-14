a516c19b81e1463ced7c1031e31ec9ad21ad6440
koeshiro <koeshiro@yandex.ru>
2021-01-14 19:10:53 +0300

    add examples

---
+++ example/server.js
@@ -0,0 +1,14 @@
+const fsp = require('fs/promises');
+const path = require('path');
+const express = require('express');
+const app = express();
+app.use('/dist',express.static(path.join(__dirname, '../dist')));
+
+app.use(express.static(__dirname));
+/*app.get('/v-ymap',async (req,res)=>{
+    res.setHeader('Content-Type','text/html');
+    res.end(await fsp.readFile(`${__dirname}/v-ymap.html`));
+});*/
+app.listen(3000,()=>{
+
+});
